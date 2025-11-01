fb134f2108742da6e2499852f6945b2528634d8e
salieri77 <hades666mdan@gmail.com>
2025-11-01 20:55:01 -0300

    Atualizada a função main para capturar n_indexes e amplitude_spectrum da série de Fourier e corrigidos os parâmetros nas funções de plotagem.

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     f0 = 1.0
     w0 = 2 * np.pi * f0
 
-    wave_list = fs.fourier_series(t, A, w0, 250, 'triangular')
+    wave_list, n_list, amp_spec = fs.fourier_series(t, A, w0, 250, 'triangular')
     wave_sum = fs.sum_waves(wave_list)
 
-    pl.plot_fourier_series(t, wave_list, fs.n_indexes)
-    pl.plot_fourier_series(t, wave_list, fs.n_indexes, True)
-    pl.plot_waves_on_same_graph(t, wave_list, fs.n_indexes)
+    pl.plot_fourier_series(t, wave_list[:20], n_list[:20])
+    pl.plot_fourier_series(t, wave_list[:20], n_list[:20], True)
+    pl.plot_waves_on_same_graph(t, wave_list, n_list)
     pl.plot_wave_sum(t, wave_sum) 
-    pl.plot_amplitude_spectrum(fs.n_indexes, fs.amplitude_spectrum)
+    pl.plot_amplitude_spectrum(n_list, amp_spec)
