c11d04544916ccefcf65e153ad06354805763c1f
salieri77 <hades666mdan@gmail.com>
2025-11-01 20:23:25 -0300

    Feita a função da série de fourier

---
+++ fourier_series.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+amplitude_spectrum = []
+n_indexes = []
+
+def fourier_series(t, A, w0, N, mode):
+    amplitude_spectrum.clear()
+    n_indexes.clear()
+    
+    wave_list = []
+    
+    if mode == 'triangular':
+        for n in range(1, N + 1):
+            n_indexes.append(n)
+            if n % 2 == 0:
+                wave_list.append(np.zeros_like(t))
+                An = 0.0
+            else:
+                An = (8 * A) / (np.pi**2 * n**2)
+                Sinn = np.sin(n * w0 * t)
+                wave_list.append(An * Sinn)
+                
+            amplitude_spectrum.append(abs(An))
+                
+    elif mode == 'square':
+        for n in range(1, N + 1):
+            n_indexes.append(n)
+            if n % 2 == 0:
+                wave_list.append(np.zeros_like(t))
+                An = 0.0
+            else:
+                An = (4 * A) / (np.pi * n)
+                Sinn = np.sin(n * w0 * t)
+                wave_list.append(An * Sinn)
+                
+            amplitude_spectrum.append(abs(An))
+                
+    return wave_list
